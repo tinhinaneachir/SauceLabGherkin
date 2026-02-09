505da7f703a208d2e0e50594ed646815c02af3e0
Tina <you@example.com>
2026-02-09 12:34:04 +0100

    add modif sur feature

--- src/test/java/com/example/automation/pages/LoginPage.java
+++ src/test/java/com/example/automation/pages/LoginPage.java
@@ -1,54 +1,51 @@
 package com.example.automation.pages;
-
-import org.openqa.selenium.By;
+import io.cucumber.java.de.Wenn;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class LoginPage extends BasePage {
+public class LoginPage extends BasePage{
 
     @FindBy(id = "user-name")
-    private WebElement usernameInput;
-
+    WebElement usernameInput;
     @FindBy(id = "password")
-    private WebElement passwordInput;
-
+    WebElement passwordInput;
     @FindBy(id = "login-button")
-    private WebElement loginButton;
-
-    private By errorMessage = By.cssSelector("h3[data-test='error']");
+    WebElement loginButton;
 
     public LoginPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement getUsernameInput() {
+        return usernameInput;
+    }
+
+    public WebElement getPasswordInput() {
+        return passwordInput;
+    }
+
+    public void login(String username, String password) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+
+        // renseigne les champs
+        // appuye sur le bouton connexion
     }
 
     public void open(String url) {
         driver.get(url);
     }
 
-    public void login(String username, String password) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    @FindBy(xpath = "//h3[@data-test='error']")
+    WebElement errorMessage;
 
-        wait.until(ExpectedConditions.visibilityOf(usernameInput));
-        usernameInput.clear();
-        usernameInput.sendKeys(username);
-
-        passwordInput.clear();
-        passwordInput.sendKeys(password);
-
-        loginButton.click();
+    public String getErrorMessage() {
+        return errorMessage.getText();
     }
 
-
-
-
-    public boolean isErrorMessageDisplayed() {
-        return !driver.findElements(errorMessage).isEmpty();
+    public boolean afficheErreur() {
+        return errorMessage.isDisplayed();
     }
 }
