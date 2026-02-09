1c9acf41e07462907b3e402edb950b1e4788d7b5
Tina <you@example.com>
2026-02-09 16:45:37 +0100

    add plan de test to jenkinsfile

--- src/test/java/com/example/automation/configuration/DriverFactory.java
+++ src/test/java/com/example/automation/configuration/DriverFactory.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
